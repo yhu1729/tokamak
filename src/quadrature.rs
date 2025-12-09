efaab98b18606738daefcaf7bcc047c3a1dda7aa
Yifan Hu <224434986+yhu1729@users.noreply.github.com>
2025-12-09 16:52:31 -0500

    test: add tests

--- src/quadrature.rs
+++ src/quadrature.rs
@@ -1,3 +1,5 @@
+use crate::assert_eq_within;
+use crate::constant;
 use tokamak::lib;
 
 pub enum QuadratureType {
@@ -31,20 +33,32 @@ mod test {
     use super::*;
 
     unsafe extern "C" fn integrate_f1(x: f64) -> f64 {
-        return x * 2.0;
+        return x * 3.0;
+    }
+
+    unsafe extern "C" fn integrate_f2(x: f64) -> f64 {
+        return 1.0 + x * 3.0 + x * x * 4.0;
     }
 
     #[test]
     fn test_integrate() {
-        let output = integrate(
+        let output_f1 = integrate(
             QuadratureType::GaussLegendre,
-            0,
+            1,
             Some(integrate_f1),
             0.0,
-            1.0,
+            3.0,
         )
         .expect("integrate");
-
-        assert_eq!(output, 1.0);
+        assert_eq!(output_f1, 13.5);
+        let output_f2 = integrate(
+            QuadratureType::GaussLegendre,
+            2,
+            Some(integrate_f2),
+            -1.0,
+            2.0,
+        )
+        .expect("integrate");
+        assert_eq_within!(output_f2, 19.5, constant::EPSILON_F64);
     }
 }
